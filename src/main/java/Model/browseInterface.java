55b3e0b2f0137f1c52fc3a79ae152e1c3cb92bc4
cthacker-udel <cthacker@udel.edu>
2021-04-01 23:43:18 -0400

    Added browse interface

--- src/main/java/Model/browseInterface.java
+++ src/main/java/Model/browseInterface.java
@@ -1,9 +1,13 @@
 package Model;
 
+import Controller.AlbumController.MultipleAlbums.BaseAlbum;
+import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 
 public interface browseInterface {
 
     @GET("https://api.spotify.com/v1/browse/new-releases")
+    Call<BaseAlbum> getNewReleases(@Header("Authorization") String authorization);
 
 }
