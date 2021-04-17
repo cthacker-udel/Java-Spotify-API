9c0e4532616e834eaa420f25cc39485e83b34d63
cthacker-udel <cthacker@udel.edu>
2021-04-17 01:47:28 -0400

    Added: Tests : Search API

---
+++ src/test/java/getRequests/SearchTest.java
@@ -0,0 +1,40 @@
+package getRequests;
+
+import Client.SpotifyClient;
+import Client.SpotifyLogin;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SearchTest {
+
+    static SpotifyClient client;
+
+    static {
+        try {
+            client = new SpotifyClient("d56c8c3f79a1459bba2c286cfa7aa15b", "9dcd475a773d467990dd75eede0af55f");
+
+            SpotifyLogin login = client.getLogin();
+            login.setEmailOrUsername("exampleEmail");
+            login.setPassword("examplePasword");
+            login.setRedirectUri("exampleRedirectURI");
+            login.addScope("user-modify-playback-state");
+            client.requestAuthCodeFlowCode(client);
+            client.generateAccessTokenAndRefreshToken(client);
+
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testSearchForAnItem() throws IOException{
+
+        assertNotNull(client.searchForItem(client));
+
+    }
+
+}
